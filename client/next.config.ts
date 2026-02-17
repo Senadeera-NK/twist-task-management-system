6654734e7742dd96ef7536609e430c1845a4fe0d
Senadeera NK <naduni.20191206@iit.ac.lk>
2026-02-17 09:22:58 +0530

    updated the desitnation url tothe .env variable in next.confug.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://twist-task-management-system.onrender.com/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
